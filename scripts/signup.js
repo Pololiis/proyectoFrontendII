4a4755393aa3e9fbd874956b87ff3a6dd2edc448
aldrinj0se <aldrinsilvam@gmail.com>
2023-09-06 23:45:30 -0300

    Cambios 06-09-23 23:45

--- scripts/signup.js
+++ scripts/signup.js
@@ -1,70 +1,71 @@
 window.addEventListener("load", function () {
-	/* ---------------------- obtenemos variables globales ---------------------- */
-    const form = document.forms[0];
-    const firstName = document.getElementById("inputNombre");
-    const lastName= document.getElementById("inputApellido");
-    const email = document.getElementById("inputEmail");
-    const password = document.getElementById("inputPassword");
-    const repeatPassword = document.getElementById("inputPasswordRepetida");
-    const url = "https://todo-api.ctd.academy/v1";
+  /* ---------------------- obtenemos variables globales ---------------------- */
+  const form = document.forms[0];
+  const firstName = document.getElementById("inputNombre");
+  const lastName = document.getElementById("inputApellido");
+  const email = document.getElementById("inputEmail");
+  const password = document.getElementById("inputPassword");
+  const repeatPassword = document.getElementById("inputPasswordRepetida");
+  const url = "https://todo-api.ctd.academy/v1";
 
-	/* -------------------------------------------------------------------------- */
-	/*            FUNCIÓN 1: Escuchamos el submit y preparamos el envío           */
-	/* -------------------------------------------------------------------------- */
-	form.addEventListener("submit", function (event) {
-        event.preventDefault();
+  /* -------------------------------------------------------------------------- */
+  /*            FUNCIÓN 1: Escuchamos el submit y preparamos el envío           */
+  /* -------------------------------------------------------------------------- */
+  form.addEventListener("submit", function (event) {
+    event.preventDefault();
 
-        if (compararContrasenias(password.value, repeatPassword.value) &&
-            validarContrasenia(password.value) &&
-            validarEmail(email.value)) {
+    if (
+      compararContrasenias(password.value, repeatPassword.value) &&
+      validarContrasenia(password.value) &&
+      validarEmail(email.value)
+    ) {
+      const payload = {
+        firstName: firstName.value,
+        lastName: lastName.value,
+        email: email.value,
+        password: password.value,
+      };
 
-            const payload = {
-                firstName: firstName.value,
-                lastName: lastName.value,
-                email: email.value,
-                password: password.value
-            }
-    
-            const settings = {
-                method: "POST",
-                body: JSON.stringify(payload),
-                headers: {
-                    'Content-Type': 'application/json'
-                }
-            }
-    
-            realizarRegister(settings);
-    
-            form.reset();
-        
-        }
-    });
+      const settings = {
+        method: "POST",
+        body: JSON.stringify(payload),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      };
 
-	/* -------------------------------------------------------------------------- */
-	/*                    FUNCIÓN 2: Realizar el signup [POST]                    */
-	/* -------------------------------------------------------------------------- */
-	function realizarRegister(settings) {
+      realizarRegister(settings);
 
-        fetch(`${url}/users`, settings)
-        .then(response => {
-            if (response.ok) {
-                return response.json();
-            } else {
-                return Promise.reject(response);
-            }
-        })
-        .then(data => {
-            if (data.jwt) {
-                localStorage.setItem("jwt", JSON.stringify(data.jwt));
-                location.replace("./mis-tareas.html");
-            }
-        })
-        .catch(err => {
-            if (err.status == 400) {
-                alert("El usuario ya se encuentra registrado / Alguno de los datos requeridos está incompleto");
-            } else {
-                alert("Error del servidor");
-            }
-        })
+      form.reset();
     }
+  });
+
+  /* -------------------------------------------------------------------------- */
+  /*                    FUNCIÓN 2: Realizar el signup [POST]                    */
+  /* -------------------------------------------------------------------------- */
+  function realizarRegister(settings) {
+    fetch(`${url}/users`, settings)
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          return Promise.reject(response);
+        }
+      })
+      .then((data) => {
+        if (data.jwt) {
+          localStorage.setItem("jwt", JSON.stringify(data.jwt));
+          location.replace("./mis-tareas.html");
+        }
+      })
+      .catch((err) => {
+        if (err.status == 400) {
+          alert(
+            "El usuario ya se encuentra registrado / Alguno de los datos requeridos está incompleto"
+          );
+        } else {
+          alert("Error del servidor");
+        }
+      });
+  }
 });
--- scripts/utils.js
+++ scripts/utils.js
@@ -1,35 +1,41 @@
 /* ---------------------------------- texto --------------------------------- */
-function validarTexto(texto) {
-}
+function validarTexto(texto) {}
 
-function normalizarTexto(texto) {
-    
-}
+function normalizarTexto(texto) {}
 
 /* ---------------------------------- email --------------------------------- */
 function validarEmail(email) {
-    let regExp = new RegExp("^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,4}");
+  let regExp = new RegExp("^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,4}");
+  if (regExp.test(email)) {
+    return true;
+  } else {
     alert("El correo es inválido.");
-    return regExp.test(email);
-    
+    return false;
+  }
 }
 
-function normalizarEmail(email) {
-    #aa
-}
+function normalizarEmail(email) {}
 
 /* -------------------------------- password -------------------------------- */
 function validarContrasenia(contrasenia) {
-    let regExp = new RegExp("^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,10}$");
-    alert("La contraseña debe tener entre 8 y 10 caracteres, al menos una mayúscula, un número y un caracter especial")
-    return regExp.test(contrasenia);
+  let regExp = new RegExp(
+    "^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,10}$"
+  );
+  if (regExp.test(contrasenia)) {
+    return true;
+  } else {
+    // alert(
+    //   "La contraseña debe tener entre 8 y 10 caracteres, al menos una mayúscula, un número y un caracter especial"
+    // );
+    return false;
+  }
 }
 
 function compararContrasenias(contrasenia_1, contrasenia_2) {
-    if (contrasenia_1 === contrasenia_2) {
-        return true;
-    } else {
-        alert("Las contraseñas no coinciden.")
-        return false;
-    }
+  if (contrasenia_1 === contrasenia_2) {
+    return true;
+  } else {
+    //alert("Las contraseñas no coinciden.");
+    return false;
+  }
 }
